e51d22c258a4ce603282e239082f32b5743af1ad
Philipp Schmitt <philipp@schmitt.co>
2021-11-19 08:28:16 +0100

    WIP: Update copy_from_zsh_src.zsh so that it handles its new location better

--- scripts/copy_from_zsh_src.zsh
+++ scripts/copy_from_zsh_src.zsh
@@ -1,13 +1,20 @@
 #!/usr/bin/env zsh
 
-[[ -z "$1" || "$1" = "-h" || "$1" = "--help" ]] && { print "Single argument: path to Zsh source tree"; exit 0; }
+# Go to root directory
+cd ${0:a:h}/..
 
-print "Will invoke git clean -dxf, 3 seconds"
-sleep 3
+[[ -z "$1" || "$1" = "-h" || "$1" = "--help" ]] && {
+  print "Single argument: path to Zsh source tree"
+  exit 0
+}
 
-git clean -dxf
+# print "Will invoke git clean -dxf, 3 seconds"
+# sleep 3 && git clean -dxf
 
-[[ ! -d "$1" ]] && { print "Path to Zsh source doesn't exist (i.e.: $1)"; exit 1; }
+[[ ! -d "$1" ]] && {
+  print "Path to Zsh source doesn't exist (i.e.: $1)"
+  exit 1
+}
 
 local from="$1"
 
@@ -17,13 +24,16 @@ colors
 integer count=0
 
 for i in configure.ac Src/*.c Src/*.h; do
-    if [[ -f "$from/$i" ]]; then
-        cp -vf "$from/$i" "$i" && (( ++ count )) || print "${fg_bold[red]}Copy error for: $i${reset_color}"
-    else
-        print "${fg[red]}$i Doesn't exist${reset_color}"
-    fi
+  if [[ -f "$from/$i" ]]; then
+    cp -vf "$from/$i" "$i" && (( ++ count )) || \
+      print "${fg_bold[red]}Copy error for: $i${reset_color}"
+  else
+    print "${fg[red]}$i Doesn't exist${reset_color}"
+  fi
 done
 
 echo "${fg[green]}Copied ${fg[yellow]}$count${fg[green]} files${reset_color}"
 
 patch -p2 -i ./patch_cfgac.diff
+
+# vim: set ft=zsh et ts=2 sw=2 :
